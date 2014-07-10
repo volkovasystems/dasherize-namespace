adf4bd3dd55d76591cfbddb093916c899b6c1988
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-07-10 18:58:52 +0800

    Added dasherize-namespace.js file.

---
+++ dasherize-namespace.js
@@ -0,0 +1,23 @@
+/*:
+	@module-configuration:
+		{
+			"packageName": "dasherize-namespace",
+			"fileName": "dasherize-namespace.js",
+			"moduleName": "dasherizeNamespace",
+			"authorName": "Richeve S. Bebedor",
+			"authorEMail": "richeve.bebedor@gmail.com",
+			"repository": "git@github.com:volkovasystems/dasherize-namespace.git",
+			"testCase": "dasherize-namespace-test.js",
+			"isGlobal": true
+		}
+	@end-module-configuration
+
+	@module-documentation:
+
+	@end-module-documentation
+*/
+var dasherizeNamespace = function dasherizeNamespace( namespace ){
+
+};
+
+( module || { } ).exports = dasherizeNamespace;
